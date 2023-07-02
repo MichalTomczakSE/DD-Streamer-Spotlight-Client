3a92d646ae09a2703e772aef77584ba24a0db1d7
Michał Tomczak <MichalTomczakSe@gmail.com>
2023-07-02 18:58:39 +0200

    Install socket.io, wrapped app in WebSocketProvider

--- src/index.tsx
+++ src/index.tsx
@@ -5,19 +5,22 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {socket, WebSocketProvider} from "../contexts/WebSocketContext";
 
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <QueryClientProvider client={queryClient}>
-        <React.StrictMode>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </React.StrictMode>
-    </QueryClientProvider>
+    <WebSocketProvider value={socket}>
+        <QueryClientProvider client={queryClient}>
+            <React.StrictMode>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </React.StrictMode>
+        </QueryClientProvider>
+    </WebSocketProvider>
 );
 
 
